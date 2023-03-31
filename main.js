a35297e492da2e8a4bd3eb3628f60819a8dfcb00
asma20rejeb <asma20rejeb@gmail.com>
2023-03-31 21:19:29 +0100

    second commit

--- main.js
+++ main.js
@@ -3,19 +3,21 @@ var thanksBox= document.querySelector(".thanks")
 var list = document.querySelector(".ul")
 var rating= document.getElementById("rating")
 const submitBtn= document.querySelector(".submit")
+const submitAgain= document.querySelector(".again")
 var rates = document.querySelectorAll(".btn")
 
 submitBtn.addEventListener("click" , () => {
     thanksBox.classList.remove("hidden")
     mainBox.classList.add("hidden")
-
+})
+submitAgain.addEventListener("click" , () => {
+    thanksBox.classList.add("hidden")
+    mainBox.classList.remove("hidden")
 })
 
 rates.forEach((rate) => {
 
-    rate.addEventListener("click" , () =>{
-        console.log(rate.innerHTML);
-        rating.innerHTML = rate.innerHTML
-    })
+  rate.addEventListener("click" , () =>{
+      rating.innerHTML = rate.innerHTML
+  })
 })
-
